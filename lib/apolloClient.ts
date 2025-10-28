bad4b48a8914769e6bf656f1daf8021fd29abd18
yaheaalani0 <yaheaalani0@gmail.com>
2025-10-28 09:54:35 +0100

    ipated site

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -3,7 +3,7 @@ import fetch from "cross-fetch";
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: "http://headless-wp.local/graphql", // your WordPress GraphQL endpoint
+    uri: process.env.NEXT_PUBLIC_WORDPRESS_API_URL || "http://headless-wp.local/graphql",
     fetch,
   }),
   cache: new InMemoryCache(),
